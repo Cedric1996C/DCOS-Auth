6cfde5665c9ae31389e00a980ed47d12eaa80390
JetMuffin <564936642@qq.com>
2017-07-12 19:31:31 +0800

    Update configuration
    Signed-off-by: JetMuffin <564936642@qq.com>

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var Config = {
-    DCOSUI_URL: 'http://dc.njuics.cn',
-    LOGINAUTH_URL: 'http://114.212.189.147:10001',
-    LOGINAUTH_PORT: 10001
+  DCOSUI_URL: 'http://dc.njuics.cn',
+    LOGINAUTH_URL: 'http://114.212.189.147:10009',
+    LOGINAUTH_PORT: 10009
 }
 module.exports = Config;
